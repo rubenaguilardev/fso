0990b9fc6d5ab8470aade3df3150ddf9028c6e66
Ruben Aguilar <ragui56@wgu.edu>
2026-05-27 02:16:28 -0700

    add post, error handling

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 const PORT = 3001
 
-
-const persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -32,6 +32,39 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find(person => person.id === id)
+  
+  person ? res.json(person) : res.status(400).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const id = String(Math.ceil(Math.random() * 100))
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({error: 'name missing'})
+  }
+
+  if (!body.number) {
+    return res.status(400).json({error: 'number missing'})
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return res.status(409).json({error: 'name must be unique'})
+  }
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: id
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
 app.get('/info', (req, res) => {
   const date = new Date()
   res.send(`
@@ -40,4 +73,10 @@ app.get('/info', (req, res) => {
   `)
 })
 
-app.listen(PORT)
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
