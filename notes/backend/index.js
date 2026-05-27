36219f2b1f20809449d859d24a0c179dfbb33c08
Ruben Aguilar <ragui56@wgu.edu>
2026-05-27 16:24:08 -0700

    add dist

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -86,7 +87,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT ||3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
