a1183b3ad9a1a6ebe314246a5c7c532ed631ddf8
Ruben Aguilar <ragui56@wgu.edu>
2026-05-31 00:41:36 -0700

    update

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
 
-app.use(express.json())
+const app = express()
 app.use(cors())
-app.use(express.static('dist'))
+app.use(express.json())
+
+const PORT = process.env || 3001
 
 let notes = [
   {
@@ -24,54 +25,37 @@ let notes = [
   }
 ]
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, res) => {
-  res.json(notes)
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome</h1>')
 })
 
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id
-  const note = notes.find(note => note.id === id)
-
+  const note = notes.find(n => n.id === id)
   note ? res.json(note) : res.status(404).end()
 })
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0 ?
+    Math.max(...notes.map(n => Number(n.id))) : 0
+  return String(maxId + 1)
+}
 
 app.post('/api/notes', (req, res) => {
   const body = req.body
   if (!body.content) {
-    return res.status(400).json({
-      error: 'content missing'
-    })
+    return res.status(400).json({ error: "content missing" })
   }
-
   const note = {
     content: body.content,
     important: body.important || false,
-    id: generateId()
+    id: generateId
   }
-
   notes = notes.concat(note)
-
   res.json(note)
 })
 
@@ -87,7 +71,4 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT ||3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
