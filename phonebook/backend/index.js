a823db5d2def8648ce2ba82ace00f2996210641e
Ruben Aguilar <ragui56@wgu.edu>
2026-06-06 23:38:51 -0700

    express, get, post, put, delete

---
+++ phonebook/backend/index.js
@@ -0,0 +1,74 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  console.log(persons)
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find(p => p.id === id)
+  person ? res.json(person) : res.status(404).end()
+})
+
+app.get('/info', (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <span>${new Date()}</span>`
+  )
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body.name) {
+    return res.status(400).json({error: 'name missing'})
+  }
+  if (!body.number) {
+    return res.status(400).json({error: 'number missing'})
+  }
+  if (persons.find(p => p.name === body.name)){
+    return res.status(409).json({error: 'person already exist'})
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 10000000)
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+const PORT = 3002
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
