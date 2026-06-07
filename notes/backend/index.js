0332987a10041b44fa1b7d185dd33e9e18bbd07a
Ruben Aguilar <ragui56@wgu.edu>
2026-06-06 22:51:51 -0700

    add get, post, delete

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -1,33 +1,39 @@
-const express = require('express')
-const cors = require('cors')
+import express from 'express'
 
 const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
 
-const PORT = process.env.PORT || 3001
+app.use(express.json())
+
 
 let notes = [
   {
-    id: "1",
-    content: "HTML is easy",
-    important: true
+    "id": "1",
+    "content": "HTML is easy",
+    "important": false
   },
   {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+    "id": "2",
+    "content": "Browser can execute only JavaScript",
+    "important": true
   },
   {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+    "id": "3",
+    "content": "GET and POST are the most important methods of HTTP protocol",
+    "important": true
+  },
+  {
+    "content": "POST is used to add data",
+    "important": true,
+    "id": "4"
   }
 ]
 
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome</h1>')
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
 app.get('/api/notes/:id', (req, res) => {
@@ -36,20 +42,15 @@ app.get('/api/notes/:id', (req, res) => {
   note ? res.json(note) : res.status(404).end()
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
-
 const generateId = () => {
-  const maxId = notes.length > 0 ?
-    Math.max(...notes.map(n => Number(n.id))) : 0
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
   return String(maxId + 1)
 }
 
 app.post('/api/notes', (req, res) => {
   const body = req.body
   if (!body.content) {
-    return res.status(400).json({ error: "content missing" })
+    return res.status(400).json({error: 'content missing'})
   }
   const note = {
     content: body.content,
@@ -62,14 +63,9 @@ app.post('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (req, res) => {
   const id = req.params.id
-  notes = notes.filter(note => note.id !== id)
+  notes = notes.filter(n => n.id !== id)
   res.status(204).end()
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+const PORT = 3001
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
